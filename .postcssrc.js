b0383262d9e8d7090128ef27806617dc9b62f6eb
张杨 <futureis404@live.com>
2019-06-07 16:10:34 +0800

    update

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,3 +1,3 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
-import ImgCutter from './src/components/ImgCutter.vue'
+import ImgCutter from '../src/components/ImgCutter.vue'
 export default ImgCutter
