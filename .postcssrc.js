9d670a3887cfe32e571661467b803d5d1df7d6b6
张杨 <futureis404@live.com>
2019-06-07 16:43:15 +0800

    update

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,3 +1,10 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
-import ImgCutter from '../src/components/ImgCutter.vue'
-export default ImgCutter
+
+module.exports = {
+  "plugins": {
+    "postcss-import": {},
+    "postcss-url": {},
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {}
+  }
+}
