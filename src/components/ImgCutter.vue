0bc28f6b25a53d1f21d6fa53ddcfb6b11cf1c175
张杨 <futureis404@live.com>
2019-06-09 01:50:50 +0800

    兼容性调整 for IE

--- src/components/ImgCutter.vue
+++ src/components/ImgCutter.vue
@@ -497,6 +497,23 @@
                 let c = this.$refs['canvas'];
                 let tempImg = new Image();
                 tempImg.src = c.toDataURL();
+                if (!HTMLCanvasElement.prototype.toBlob) {
+                    Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+                        value: function (callback, type, quality) {
+                            var canvas = c;
+                            setTimeout(function() {
+                                var binStr = atob(canvas.toDataURL(type, quality).split(',')[1]);
+                                var len = binStr.length, arr = new Uint8Array(len);
+
+                                for (var i = 0; i < len; i++ ) {
+                                    arr[i] = binStr.charCodeAt(i);
+                                }
+
+                                callback(new Blob([arr], {type: type || 'image/png'}));
+                            });
+                        }
+                    });
+                }
                 c.toBlob((blob) => {
                     let reader = new FileReader();
                     reader.readAsDataURL(blob);
